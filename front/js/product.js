6aa33b16abc0607d2e199c085cd17faa2a629eab
balnelius11 <balnelius11@gmail.com>
2023-02-20 18:39:56 +0100

    mise a jour

--- front/js/product.js
+++ front/js/product.js
@@ -1,49 +1,42 @@
 const product = window.location.search.split('?id=').join('')
+// récupérer l'ID, retirer les autres caractère et le 2ème élément de l'array
 let productData = [];
+// définir productData
 const fetchproduit = async () => {
+  //créer la fonction asynchrone pour récupérer les données
     await fetch(`http://localhost:3000/api/products/${product}`)
+    // récupérer le lien de l'api et y adjoindre l'id pour retrouver le produit
     .then((response) => response.json())
+    // mettre la réponse au format json
     .then((promise) => {
     productData = promise;
     }
+    //mettre la promesse dans la variable productData
 );
 };
 const produitDispo = async() => {
     await fetchproduit();
+    // !important pour récupérer productdata
     console.log(productData)
+    //vérifier productData
     document.querySelector(".item__img").innerHTML = `<img src="${productData.imageUrl}" alt="${productData.altTxt}" />`;
+    //ajouter dans la section item image les données  correspondant a l'image et au texte alternatif
     document.getElementById('title').innerText = productData.name;
+    //même chose que le précédent mais pour ajouter le titre dans les données
     document.getElementById('price').innerText = productData.price;
+    //même chose que le précédent mais pour ajouter le titre
     document.getElementById('description').innerText = productData.description;
+    //même chose que le précédent mais pour ajouter la description
     const colorsList = productData.colors.map((color) => {
+      //création de la nouvelle variable  pour créer un tableau de la liste des couleurs
         let option = document.createElement('option');
+        //création de l'élément option
         option.value = color;
+        //donner une valeur a la couleur
         option.innerText = color;
+        //insérer la couleur sous forme de texte
         document.getElementById('colors').appendChild(option);
-      });
-      
-      document.getElementById('addToCart').addEventListener('click', (event) => {
-        event.preventDefault();
-        const quantity = document.getElementById('quantity').value;
-        const color = document.getElementById('colors').value;
-        if (quantity > 0 && color !== '') {
-          const cart = {
-            product: productData.name,
-            quantity: quantity,
-            color: color,
-            price: productData.price,
-          };
-          const cartString = localStorage.getItem('cart');
-          let cartArray;
-          if (cartString == null) {
-            cartArray = Array();
-          } else {
-            cartArray = JSON.parse(cartString);
-          }
-          cartArray.push(cart);
-          localStorage.setItem('cart', JSON.stringify(cartArray));
-          window.location = './cart.html';
-        }
+        //prendre l'élément par id color et lui mettre en enfant option qu'on viens de créer
       });
 };
 
